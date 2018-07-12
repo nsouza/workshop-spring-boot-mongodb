40a609e856a1facf0d11f623ec3796ff25397470
nsouza <nilo.contatos@gmail.com>
2018-07-11 22:41:57 -0300

    Primeiro commit - projeto criado

---
+++ src/main/java/com/nelioalves/workshopmogo/WorkshopmogoApplication.java
@@ -0,0 +1,12 @@
+package com.nelioalves.workshopmogo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopmogoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopmogoApplication.class, args);
+	}
+}
---
+++ src/test/java/com/nelioalves/workshopmogo/WorkshopmogoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.nelioalves.workshopmogo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WorkshopmogoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
